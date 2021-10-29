f5c5b66b1cf754ec60351dcbf8beda038f8297c3
sarahandreas <sarah.andreas@gmail.com>
2021-10-29 11:34:48 +1100

    fixed buttons

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,12 +3,20 @@ import Name from './Name'
 import Nav from './Nav'
 import Shapes from './Shapes'
 
+import useWindowSize from '../hooks/useWindowSize'
+
 export default function Hero() {
+
+    let { width } = useWindowSize()
+
+    console.log(width)
+
     return (
         <header>
-            <Nav />
+            { width >= 770 && <Nav /> }
             <Name />
             <Shapes />
+            { width < 770 && <Nav /> }
         </header>
     )
 }
